e4530f727a61f0257ceffe39a74230a277d0d9d0
Bruno <brunocosta.1991@hotmail.com>
2024-11-10 20:09:45 -0300

    resolution

--- desafio.py
+++ desafio.py
@@ -198,7 +198,7 @@ def recuperar_conta_cliente(cliente):
         print("\n@@@ Cliente não possui conta! @@@")
         return
 
-    # FIXME: não permite cliente escolher a conta
+    # FIXME: client not permitted select account
     return cliente.accounts[0]
 
 
